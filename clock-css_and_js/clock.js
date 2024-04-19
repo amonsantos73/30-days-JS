490071be31f6b6706565810ff25b2cc7f438abd3
amonsantos73 <amonlemes3@gmail.com>
2024-04-19 14:12:24 -0300

    adding de clock with js and css to the project

---
+++ clock-css_and_js/clock.js
@@ -0,0 +1,28 @@
+const secondHand = document.querySelector('.seconds-hand');
+const minsHand = document.querySelector('.minutes-hand');
+const hourHand = document.querySelector('.hours-hand');
+const digitalClock = document.querySelector('.digital-clock-value');
+
+function setDate() {
+
+  const now = new Date();
+
+  const seconds = now.getSeconds();
+  const secondsDegrees = ((seconds / 60) * 360) + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+  const mins = now.getMinutes();
+  const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
+  minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+  const hour = now.getHours();
+  const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+
+  digitalClock.innerHTML = `${hour.toString().padStart(2, '0')}:${mins.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+
+}
+
+setInterval(setDate, 1000);
+
+setDate();
